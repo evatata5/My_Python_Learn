9699eb1d551888cbb871b02eb97df04da70aefc2
evatata5 <tacy0505@gmail.com>
2025-01-25 00:20:26 +0300

    page number

---
+++ test.py
@@ -0,0 +1,12 @@
+def main():
+  total_symbol = 1578
+  total_page = 0
+  symbol = 0
+  while symbol < total_symbol:
+     total_page += 1
+     symbol += len(str(total_page))
+  print(f"Всего страниц {total_page}")
+  print(f"Всего символов {symbol}")
+  
+if __name__ == "__main__":
+  main()
